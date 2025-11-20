3d0a843f84ff30b4c84a9ea4be3987a3f45e0f2d
Vy Nguyen <vych3rry@hotmail.com>
2025-11-20 17:54:23 -0500

    Added db context (I think)

---
+++ ClinicManagement_proj/DAL/ClinicDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ClinicManagement_proj.DAL
+{
+    internal class ClinicDbContext : DbContext
+    {
+
+        public ClinicDbContext() : base("name=HealthCareClinicDBEntities")
+        {
+        }
+    }
+}
